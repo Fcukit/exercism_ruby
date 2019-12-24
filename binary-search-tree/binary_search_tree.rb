9e179f4282bd199232c94404821f5af0d409336b
Vlasov Viktor <vvvlasov@activebc.ru>
2019-12-25 00:31:54 +0300

    binary tree search

--- binary-search-tree/binary_search_tree.rb
+++ binary-search-tree/binary_search_tree.rb
@@ -1,8 +1,59 @@
-=begin
-Write your code for the 'Binary Search Tree' exercise in this file. Make the tests in
-`binary_search_tree_test.rb` pass.
+class Bst
+  attr_reader :root
 
-To get started with TDD, see the `README.md` file in your
-`ruby/binary-search-tree` directory.
-=end
+  def initialize(x)
+    @root = Node.new(x)
+  end
 
+  def data
+    root.data
+  end
+
+  def left
+    root.left
+  end
+
+  def right
+    root.right
+  end
+
+  def each(&block)
+    all_data(root).each &block
+  end
+
+  def all_data(root)
+    return [] unless root
+    all_data(root.left) + [root.data] + all_data(root.right)
+  end
+
+  def insert(x)
+    current = root
+    while current
+      if x <= current.data
+        if current.left
+          current = current.left
+        else
+          current.left = Node.new(x)
+          return
+        end
+      else
+        if current.right
+          current = current.right
+        else
+          current.right = Node.new(x)
+          return
+        end
+      end
+    end
+  end
+
+  class Node
+    attr_accessor :data, :left, :right
+
+    def initialize(x)
+      @data = x
+      @left = nil
+      @right = nil
+    end
+  end
+end
